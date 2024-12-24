08b3cfe14233305942e69ed93a09db9333de8e55
rafat22 <rafat22@navgurukul.org>
2024-12-24 22:47:49 +0530

    try to resolved

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Ignore certain warnings
+        if (warning.code === 'THIS_IS_UNDEFINED') return;
+        // Use default for everything else
+        warn(warning);
+      }
+    }
+  }
+});
